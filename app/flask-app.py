41bcd1b437682fbe50cd16af2277510e9e3f1eae
lucanuscervus <you@example.com>
2019-09-20 16:49:59 +0200

    minor fixes

--- app/flask-app.py
+++ app/flask-app.py
@@ -2,7 +2,7 @@ import socket
 from flask import Flask, render_template, escape, request
 
 app = Flask(__name__)
-version = "0.3"
+version = "0.4"
 
 def return_hostname():
   return socket.gethostname()
@@ -11,14 +11,9 @@ def get_ip(hostname):
   return socket.gethostbyname(hostname)
 
 @app.route('/')
-def hello():
-  name = request.args.get("name", "World")
-  return f'Hello, {escape(name)}! again'
-
-@app.route('/about')
-def about():
+def flaskapp():
   hostname = return_hostname()
   ip = get_ip(hostname)
   # return "Hostname is: {}".format(socket.gethostname())
   #return f'Here is some information about me'
-  return render_template('bulma.html',hostname=hostname,ip=ip,version=version)
+  return render_template('index.html',hostname=hostname,ip=ip,version=version)
